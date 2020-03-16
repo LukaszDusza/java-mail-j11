dec420b48bddb03d12fe142080e4c68c17329deb
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-03-16 18:31:42 +0100

    init

---
+++ src/main/java/akademia/MailSenderJavaApplication.java
@@ -0,0 +1,14 @@
+package akademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MailSenderJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MailSenderJavaApplication.class, args);
+  }
+
+}
